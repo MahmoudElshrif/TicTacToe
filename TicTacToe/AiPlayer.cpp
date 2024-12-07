db02fef15bd5e50c7e9675564a4b21d35108a2a7
mahmoudElshrif <mahmoudfathyelshrif@gmail.com>
2024-12-07 21:51:53 +0200

    part of the search function

--- TicTacToe/AiPlayer.cpp
+++ TicTacToe/AiPlayer.cpp
@@ -1,1 +1,61 @@
 #include "AiPlayer.h"
+
+
+
+
+AiPlayer::AiPlayer(string name, char symbol, bool isPlayerOne) : Player(name, symbol), isPlayerOne(isPlayerOne) {
+	tempboard = new int* [3];
+	for (int i = 0; i < 3; i++) {
+		tempboard[i] = new int[3] {-1,-1,-1};
+	}
+}
+
+void AiPlayer::getmove(int& x, int& y) {
+	//constructs a temp board to do search on
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			//-1 empty
+			//0 for player one
+			//1 for player 2
+			auto symb = tolower(board[x][y]);
+			if (symb == symbols[0]) {
+				tempboard[x][y] = 0;
+			}
+			else if (symbol == symbols[!isPlayerOne]) {
+				tempboard[x][y] = 1;
+			}
+			else {
+				tempboard[x][y] = -1;
+			}
+		}
+	}
+
+	search(x, y, isPlayerOne);
+}
+
+
+int AiPlayer::search(int& x, int& y,bool isPlayerOne,int depth) {
+	int eval();
+	if (depth == 0 || eval != 0) {
+		return eval();
+	}
+
+	int currentmax = INT_MIN;
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			char temp = board[i][j];
+			if (temp == symbols[0] || temp == symbols[1]) continue;
+			board[i][j] = symbol;
+			//gets the opposite of the evalution 
+			int eval = -search(x, y,!isPlayerOne,depth - 1);
+			board[i][j] = temp;
+			if (currentmax < eval) {
+				x = i;
+				y = j;
+				currentmax = eval;
+			}
+		}
+	}
+
+	return currentmax;
+}
--- TicTacToe/AiPlayer.h
+++ TicTacToe/AiPlayer.h
@@ -5,12 +5,15 @@ class AiPlayer : public Player<char>
 {
 
 	char** board;
+	int** tempboard;
 	bool isPlayerOne;
 
 public:
-	AiPlayer(string name, char symbol, bool isPlayerOne) : Player(name, symbol), isPlayerOne(isPlayerOne) {}
-	void setBoard(char** b, int sizex, int sizey) { for (int x = 0; x < sizex; x++) for (int y = 0; y < sizey; y++) board[x][y] = b[x][y]; }
+	char symbols[2] = { 'x','o' };
+	AiPlayer(string name, char symbol, bool isPlayerOne);
+	void setBoard(char** b, int sizex, int sizey) { board = b; }
 	int virtual eval() = 0;
-	int virtual search(int depth) = 0;
+	int virtual search(int& x,int& y,bool isPlayerOne,int depth = 3);
+	void getmove(int& x,int& y) override;
 };
 
