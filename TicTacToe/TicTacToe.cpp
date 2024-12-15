748c7f1a8a8df87aaad48b935847704f11959aaa
mahmoudElshrif <mahmoudfathyelshrif@gmail.com>
2024-12-15 03:29:11 +0200

    numerical and four in row

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -3,6 +3,8 @@
 #include "FiveTicTacToe.h"
 #include "BigTicTacToe.h"
 #include "SusGame.h"
+#include "Four-in-a-row.h"
+#include "Numerical Tic-Tac-Toe.h"
 
 using namespace std;
 
@@ -263,6 +265,121 @@ void sus() {
     }
 }
 
+void four() {
+    string choice;
+    Player<char>* players[2];
+    Board<char>* B = new Four_in_a_row_Board<char>();
+    string player1Name, player2Name;
+    cout << "Welcome to FCAI Four-In-A-Row Game. :)\n";
+    // Set up player 1
+    cout << "Choose Player1 (X) type:\n";
+    cout << "1. Human\n";
+    cout << "2. Random Computer\n";
+    cin >> choice;
+
+    if (choice == "1") {
+        cout << "Enter name of this player1: ";
+        cin.ignore();
+        getline(cin, player1Name);
+        players[0] = new Four_in_a_row_Player<char>(player1Name, 'X');
+    }
+    else if (choice == "2") {
+        cout << "Enter name of this player1: ";
+        players[0] = new Four_in_a_row_Random_Player<char>('X');
+    }
+    else {
+        cout << "Invalid choice for Player 1. Exiting the game.\n";
+        return;
+    }
+
+    // Set up player 2
+    cout << "Choose Player2 (O) type:\n";
+    cout << "1. Human\n";
+    cout << "2. Random Computer\n";
+    cin >> choice;
+
+    if (choice == "1") {
+        cout << "Enter name of this player2: ";
+        cin.ignore();
+        getline(cin, player2Name);
+        players[1] = new Four_in_a_row_Player<char>(player2Name, 'O');
+    }
+    else if (choice == "2") {
+        cout << "Enter name of this player2: ";
+        players[1] = new Four_in_a_row_Random_Player<char>('O');
+    }
+    else {
+        cout << "Invalid choice for Player 2. Exiting the game.\n";
+        return;
+    }
+
+    // Create the game manager and run the game
+    GameManager<char> x_o_game(B, players);
+    x_o_game.run();
+
+    // Clean up
+    delete B;
+    for (int i = 0; i < 2; ++i) {
+        delete players[i];
+    }
+    return;
+}
+
+void numerical() {
+    string choice;
+    Numerical_Player<int>* players[2];
+    Board<int>* B = new Numerical_Board<int>();
+    string player1Name, player2Name;
+    cout << "Welcome to FCAI Numerical Tic-Tac-Toe!\n";
+    cout << "Choose The First Player Chooses From odd numbers only (1, 3, 5, 7, 9) type:\n1. Human\n2. Random Computer\n";
+    cin >> choice;
+    // Player 1 setup
+    if (choice == "1") {
+        cout << "Enter The First PLayer name: ";
+        cin.ignore();
+        getline(cin, player1Name);
+        players[0] = new Numerical_Player<int>(player1Name, false);
+    }
+    else if (choice == "2") {
+        cout << "Enter The Random First Player name: ";
+        players[0] = new Numerical_Random_Player<int>(0);
+    }
+    else {
+        cout << "Invalid Choice For First Player. Exiting The Game.\n";
+        return;
+    }
+    // Player 2 setup;
+    cout << "Choose The Second Player Chooses From Even Numbers only (2, 4, 6, 8) type:\n1. Human\n2. Random Computer\n";
+    cin >> choice;
+    if (choice == "1") {
+        cout << "Enter The Second PLayer name: ";
+        cin.ignore();
+        getline(cin, player2Name);
+        players[1] = new Numerical_Player<int>(player2Name, true);
+    }
+    else if (choice == "2") {
+        cout << "Enter The Random Second Player name: ";
+        players[1] = new Numerical_Random_Player<int>(0);
+    }
+    else {
+        cout << "Invalid Choice For Second Player. Exiting The Game.\n";
+        return;
+    }
+
+    players[0]->isEven = false;
+    players[1]->isEven = true;
+
+    // Create the game manager
+    Player<int>* p[2] = { static_cast<Player<int>*>(players[0]),static_cast<Player<int>*>(players[1]) };
+
+    GameManager<int> game(static_cast<Board<int>*>(B), p);
+    game.run();
+    // Cleanup
+    delete B;
+    delete players[0];
+    delete players[1];
+}
+
 int main() {
 
     cout << "1) Inverse Tic Tac Toe\n";
@@ -285,6 +402,12 @@ int main() {
         break;
     case 4:
         sus();
+        break;
+    case 5:
+        four();
+        break;
+    case 6:
+        numerical();
         break;
     }
 
