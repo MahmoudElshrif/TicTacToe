0e376a7f3feecc6ec07c70c08cce14d771faf982
mahmoudElshrif <mahmoudfathyelshrif@gmail.com>
2024-12-15 03:45:43 +0200

    FINALLY, HOLY ****

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -5,6 +5,9 @@
 #include "SusGame.h"
 #include "Four-in-a-row.h"
 #include "Numerical Tic-Tac-Toe.h"
+#include "3x3X_O.h"
+#include "pyramic.h"
+#include "wordic.h"
 
 using namespace std;
 
@@ -380,35 +383,170 @@ void numerical() {
     delete players[1];
 }
 
+void wordic() {
+    int choice;
+    Player<char>* players[2];
+    Wordic_Board<char>* B = new Wordic_Board<char>();
+    string playerXName, player2Name;
+
+    cout << "Welcome to FCAI X-O Game. :)\n";
+
+    // Set up player 1
+    cout << "Enter Player X name: ";
+    cin >> playerXName;
+    cout << "Choose Player X type:\n";
+    cout << "1. Human\n";
+    cout << "2. Random Computer\n";
+    cin >> choice;
+
+    switch (choice) {
+    case 1:
+        players[0] = new Wordic_Player<char>(playerXName, 'X');
+        break;
+    case 2:
+        players[0] = new X_O_Random_Player<char>('X');
+        break;
+    default:
+        cout << "Invalid choice for Player 1. Exiting the game.\n";
+        return;
+    }
+
+    // Set up player 2
+    cout << "Enter Player 2 name: ";
+    cin >> player2Name;
+    cout << "Choose Player 2 type:\n";
+    cout << "1. Human\n";
+    cout << "2. Random Computer\n";
+    cin >> choice;
+
+    switch (choice) {
+    case 1:
+        players[1] = new Wordic_Player<char>(player2Name, 'O');
+        break;
+    case 2:
+        players[1] = new X_O_Random_Player<char>('O');
+        break;
+    default:
+        cout << "Invalid choice for Player 2. Exiting the game.\n";
+        return;
+    }
+
+    // Create the game manager and run the game
+    GameManager<char> x_o_game(B, players);
+    x_o_game.run();
+
+    // Clean up
+    delete B;
+    for (int i = 0; i < 2; ++i) {
+        delete players[i];
+    }
+
+    return;
+}
+
+void pyramic() {
+    int choice;
+    Player<char>* players[2];
+    PyramicBoard<char>* B = new PyramicBoard<char>();
+    string playerXName, player2Name;
+
+    cout << "Welcome to FCAI X-O Game. :)\n";
+
+    // Set up player 1
+    cout << "Enter Player X name: ";
+    cin >> playerXName;
+    cout << "Choose Player X type:\n";
+    cout << "1. Human\n";
+    cout << "2. Random Computer\n";
+    cin >> choice;
+
+    switch (choice) {
+    case 1:
+        players[0] = new X_O_Player<char>(playerXName, 'X');
+        break;
+    case 2:
+        players[0] = new X_O_Random_Player<char>('X');
+        break;
+    default:
+        cout << "Invalid choice for Player 1. Exiting the game.\n";
+        return;
+    }
+
+    // Set up player 2
+    cout << "Enter Player 2 name: ";
+    cin >> player2Name;
+    cout << "Choose Player 2 type:\n";
+    cout << "1. Human\n";
+    cout << "2. Random Computer\n";
+    cin >> choice;
+
+    switch (choice) {
+    case 1:
+        players[1] = new X_O_Player<char>(player2Name, 'O');
+        break;
+    case 2:
+        players[1] = new X_O_Random_Player<char>('O');
+        break;
+    default:
+        cout << "Invalid choice for Player 2. Exiting the game.\n";
+        return;
+    }
+
+    // Create the game manager and run the game
+    GameManager<char> x_o_game(B, players);
+    x_o_game.run();
+
+    // Clean up
+    delete B;
+    for (int i = 0; i < 2; ++i) {
+        delete players[i];
+    }
+
+    return;
+}
+
 int main() {
 
-    cout << "1) Inverse Tic Tac Toe\n";
-    cout << "2) 5x5 Tic Tac Toe\n";
-    cout << "3) Big Tic Tac Toe\n";
-    cout << "4) Sus game\n";
+    cout << "1) Pyramic Tic-Tac-Toe\n";
+    cout << "2) Four-in-a-row\n";
+    cout << "3) 5 x 5 Tic Tac Toe\n";
+    cout << "4) Word Tic-tac-toe\n";
+    cout << "5) Numerical Tic-Tac-Toe\n";
+    cout << "6) Misere Tic Tac Toe\n";
+    cout << "7) Ultmate Tic Tac Toe\n";
+    cout << "8) Sus game\n";
 
     int op;
     cin >> op;
 
     switch (op) {
     case 1:
-        reverse();
+        pyramic();
         break;
     case 2:
-        fiveboard();
-        break;
-    case 3:
-        bigboard();
-        break;
-    case 4:
-        sus();
-        break;
-    case 5:
         four();
         break;
-    case 6:
+    case 3:
+        fiveboard();
+        break;
+    case 4:
+        wordic();
+        break;
+    case 5:
         numerical();
         break;
+    case 6:
+        reverse();
+        break;
+    case 7:
+        bigboard();
+        break;
+    case 8:
+        sus();
+        break;
+    default:
+        break;
+
     }
 
     return 0;
---
+++ TicTacToe/pyramic.h
@@ -0,0 +1,87 @@
+//
+// Created by pxlman on 12/5/24.
+//
+
+#ifndef XOGAME_PYRAMIC_H
+#define XOGAME_PYRAMIC_H
+#include "BoardGame_Classes.h"
+#include <iomanip>
+#include <cctype>
+
+template<typename T>
+class PyramicBoard : public Board<T> {
+private:
+    bool validMove(int& x, int& y) {
+        if (y == 0 && x == 2) return true;
+        if (y == 1 && (x <= 3 && x >= 1)) return true;
+        if (y == 2 && (x <= 4 && x >= 0)) return true;
+        return false;
+    }
+public:
+    PyramicBoard() {
+        this->rows = 3;
+        this->columns = 5;
+        this->board = new char* [this->rows];
+        for (int y = 0; y < this->rows; ++y) {
+            this->board[y] = new char[this->columns];
+            for (int x = 0; x < this->columns; ++x) {
+                this->board[y][x] = 0;
+            }
+        }
+        this->n_moves = 0;
+    }
+    bool update_board(int x, int y, T symbol) {
+        // Only update if move is valid
+        if (validMove(x, y) && (this->board[y][x] != symbol)) {
+            if (symbol == 0) {
+                this->n_moves--;
+                this->board[y][x] = 0;
+            }
+            else {
+                this->n_moves++;
+                this->board[y][x] = toupper(symbol);
+            }
+            return true;
+        }
+        else return false;
+    }
+    void display_board() {
+        for (int y = 0; y < this->rows; y++) {
+            cout << "\n";
+            for (int x = 0; x < 2 - y; x++) {
+                cout << "  " << ' ' << " " << ' ' << " ";
+                cout << setw(2) << ' ' << "  ";
+            }
+            for (int x = 2 - y; x <= 2 + y; x++) {
+                cout << " (" << x << "," << y << ")";
+                cout << setw(2) << this->board[y][x] << " |";
+            }
+        }
+        cout << "\n\\---------------------------------------------/";
+        cout << endl;
+    }
+    bool is_win() {
+        // Check middle col
+        if (this->board[0][2] == this->board[1][2] && this->board[2][2] == this->board[1][2] && this->board[1][2] != 0) return true;
+        // Check rows
+        if (this->board[1][2] == this->board[1][1] && this->board[1][1] == this->board[1][3] && this->board[1][3] != 0) return true;
+        if (this->board[2][2] == this->board[2][1] && this->board[2][1] == this->board[2][3] && this->board[2][3] != 0) return true;
+        if (this->board[2][0] == this->board[2][1] && this->board[2][1] == this->board[2][2] && this->board[2][2] != 0) return true;
+        if (this->board[2][4] == this->board[2][3] && this->board[2][3] == this->board[2][2] && this->board[2][2] != 0) return true;
+        // Check diagonals
+        if (this->board[0][2] == this->board[1][3] && this->board[1][3] == this->board[2][4] && this->board[2][4] != 0) return true;
+        if (this->board[0][2] == this->board[1][1] && this->board[1][1] == this->board[2][0] && this->board[2][0] != 0) return true;
+
+        return false;
+    }
+    bool is_draw() {
+        return (this->n_moves == 9 && !is_win());
+    }
+    bool game_is_over() {
+        return is_win() || is_draw();
+    }
+};
+
+
+
+#endif //XOGAME_PYRAMIC_H
